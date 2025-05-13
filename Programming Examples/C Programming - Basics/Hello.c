12b33c366a59e3f05d32242d789374678a914b55
Dev-team2025 <internshipteam.program@gmail.com>
2025-05-13 12:51:29 +0530

    HelloWorld-C

---
+++ Programming Examples/C Programming - Basics/Hello.c
@@ -0,0 +1,16 @@
+**Question : **Write a C program to display the message `"Hello World!"` on the screen.
+
+                 **Solution : **
+
+#include <stdio.h>
+
+                              int
+                              main()
+{
+    printf("Hello World!\n");
+    return 0;
+}
+
+## #**Expected Output : **
+``` Hello World !
+```
